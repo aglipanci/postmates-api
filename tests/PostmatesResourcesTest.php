4f0223ff3a2283da7777e96353a80e407bcd79ae
aglipanci <agli.panci@gmail.com>
2020-04-16 19:12:09 +0200

    :white_check_mark: Fixing tests.

--- tests/PostmatesResourcesTest.php
+++ tests/PostmatesResourcesTest.php
@@ -49,10 +49,10 @@ class PostmatesResourcesTest extends PHPUnit_Framework_TestCase
             'manifest' => 'test manifest',
             'pickup_name' => 'test pickup nanme',
             'pickup_address' => '222 Townsend Street, San Francisco, CA',
-            'pickup_phone_number' => '222 551 1234',
+            'pickup_phone_number' => '415-555-1234',
             'dropoff_name' => 'test dropoff name',
             'dropoff_address' => '224 Townsend Street, San Francisco, CA',
-            'dropoff_phone_number' => '222 555 5432',
+            'dropoff_phone_number' => '415-555-1234',
         ];
 
         $delivery_response = $delivery->create($params);
@@ -66,10 +66,24 @@ class PostmatesResourcesTest extends PHPUnit_Framework_TestCase
 
         $delivery = new Delivery($this->_postmates_client);
 
-        $delivery_response = $delivery->get('del_LAPCo_EAxDv6z-');
+        $params = [
+            'manifest' => 'test manifest',
+            'pickup_name' => 'test pickup nanme',
+            'pickup_address' => '222 Townsend Street, San Francisco, CA',
+            'pickup_phone_number' => '415-555-1234',
+            'dropoff_name' => 'test dropoff name',
+            'dropoff_address' => '224 Townsend Street, San Francisco, CA',
+            'dropoff_phone_number' => '415-555-1234',
+        ];
+
+        $new_delivery_response = $delivery->create($params);
+
+        $this->assertArrayHasKey('id', $new_delivery_response);
+
+        $delivery_response = $delivery->get($new_delivery_response['id']);
 
         $this->assertArrayHasKey('id', $delivery_response);
-        $this->assertEquals('del_LAPCo_EAxDv6z-', $delivery_response['id']);
+        $this->assertEquals($new_delivery_response['id'], $delivery_response['id']);
 
     }
 
